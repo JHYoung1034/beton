c81d8bbb0d370e6ed3de2b90fa92023105bc881c
yjh <1105180740@qq.com>
2025-01-21 11:12:47 +0800

    Add some empty file

---
+++ tools/network/Socket.h
@@ -0,0 +1,12 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+namespace beton {
+
+class Socket {
+
+
+
+};
+}
+#endif  //__SOCKET_H__
---
+++ tools/threadpool/threadpool.h
@@ -0,0 +1,11 @@
+#ifndef __THREAD_POOL_H__
+#define __THREAD_POOL_H__
+
+namespace beton {
+
+class ThreadPool {
+
+};
+
+}
+#endif  //__THREAD_POOL_H__
